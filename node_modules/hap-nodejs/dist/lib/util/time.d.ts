6aecad0da8aa92d9ecb674dbb597931e241537f1
xXxEdyxXx <adam.stupka@gmail.com>
2026-07-05 13:17:12 +0200

    Clean up repo and add gitignore

--- node_modules/hap-nodejs/dist/lib/util/time.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @group Utils
- */
-export declare function epochMillisFromMillisSince2001_01_01(millis: number): number;
-/**
- * @group Utils
- */
-export declare function epochMillisFromMillisSince2001_01_01Buffer(millis: Buffer): number;
-//# sourceMappingURL=time.d.ts.map
